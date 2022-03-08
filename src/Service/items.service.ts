baaf4d43d9864b5721c385c0ecdb3c30907b6ab3
ujjwal0517 <52357829+ujjwal0517@users.noreply.github.com>
2022-03-08 17:30:48 +0545

    Update items.service.ts

--- src/Service/items.service.ts
+++ src/Service/items.service.ts
@@ -12,3 +12,28 @@ export const findById = async (id: number) => {
 };
 
 // TODO Create new functions: add, update and delete
+// add data
+export const newDatas = async() =>{
+ let newData = {
+   id: req.params.id,
+   title: req.body.title,
+   body: req.body.body,
+ };
+ mockItems.push(newData);
+  return promise.resolve(newData);
+}
+
+export const editData = async(id: number) =>{
+  if(mockItems[id])
+  {
+    mockItems[id].title = req.body.title;
+    mockItems[id].body = req.body.body;
+    return Promise.resolve(mockItems);
+  }
+}
+export const deleteData = async(id: number) =>{
+if(mockItems[id]){
+  mockItems.filter((item)=>item!=mockItems[id])
+}
+
+}
